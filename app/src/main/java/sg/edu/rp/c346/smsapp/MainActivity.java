47898ead9368344addc52298c3f0a6f15601afae
OH SHI JIE <17010578@rp.edu.sg>
2018-08-15 15:08:42 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/smsapp/MainActivity.java
@@ -0,0 +1,101 @@
+package sg.edu.rp.c346.smsapp;
+
+import android.Manifest;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.pm.PackageManager;
+import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button btnSend;
+    EditText etTo;
+    EditText etContent;
+    Button btnSend2;
+
+    BroadcastReceiver br = new AnotherMessageReceiver();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnSend = findViewById(R.id.button);
+        etContent = findViewById(R.id.editTextContent);
+        etTo = findViewById(R.id.editTextTo);
+        btnSend2 = findViewById(R.id.buttonSend);
+
+        IntentFilter filter = new IntentFilter(ConnectivityManager.CONNECTIVITY_ACTION);
+        filter.addAction("android.provider.Telephony.SMS_RECEIVED");
+        this.registerReceiver(br,filter);
+
+        btnSend.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            String to = etTo.getText().toString();
+            String content = etContent.getText().toString();
+                SmsManager smsManager = SmsManager.getDefault();
+                smsManager.sendTextMessage(to, null, content, null, null);
+                Toast.makeText(getBaseContext(), "Message Sent", Toast.LENGTH_SHORT).show();
+                etContent.setText(null);
+
+            }
+        });
+        btnSend2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (Build.VERSION.SDK_INT < Build.VERSION_CODES.KITKAT) {
+                    String recipient = etTo.getText().toString();
+                    String content = etContent.getText().toString();
+                    Uri smsUri = Uri.parse("tel:" + recipient);
+                    Intent intent = new Intent(Intent.ACTION_VIEW, smsUri);
+                    intent.putExtra("address", recipient);
+                    intent.putExtra("sms_body", content);
+                    intent.setType("vnd.android-dir/mms-sms");
+                    if (intent.resolveActivity(getPackageManager()) != null) {
+                        startActivity(intent);
+                    }
+
+                }else{
+                    String to = etTo.getText().toString();
+                    String message = etContent.getText().toString();
+                    Intent intent = new Intent(Intent.ACTION_VIEW);
+                    intent.setData(Uri.parse("sms:" + to));
+                    intent.putExtra("sms_body", message);
+                    startActivity(intent);
+                }
+
+            }
+        });
+
+
+
+
+    }
+
+    private void checkPermission() {
+        int permissionSendSMS = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.SEND_SMS);
+        int permissionRecvSMS = ContextCompat.checkSelfPermission(this,
+                Manifest.permission.RECEIVE_SMS);
+        if (permissionSendSMS != PackageManager.PERMISSION_GRANTED &&
+                permissionRecvSMS != PackageManager.PERMISSION_GRANTED) {
+            String[] permissionNeeded = new String[]{Manifest.permission.SEND_SMS,
+                    Manifest.permission.RECEIVE_SMS};
+            ActivityCompat.requestPermissions(this, permissionNeeded, 1);
+        }
+    }
+}
